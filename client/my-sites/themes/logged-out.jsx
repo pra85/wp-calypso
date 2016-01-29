1d6a5e1b4f943ad58852b66c243885cd8ace5501
Bernhard Reiter <ockham@raz.or.at>
2016-01-29 21:50:38 +0100

    Fix logged-out

--- client/my-sites/themes/logged-out.jsx
+++ client/my-sites/themes/logged-out.jsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 var React = require( 'react' ),
-	bindActionCreators = require( 'redux' ).bindActionCreators,
 	connect = require( 'react-redux' ).connect,
 	pick = require( 'lodash/object/pick' ),
 	merge = require( 'lodash/object/merge' );
@@ -13,41 +12,22 @@ var React = require( 'react' ),
 var Main = require( 'components/main' ),
 	Action = require( 'state/themes/actions' ),
 	ThemePreview = require( './theme-preview' ),
-	ThemesSiteSelectorModal = require( './themes-site-selector-modal' ),
 	ThemesSelection = require( './themes-selection' ),
 	ThemeHelpers = require( 'lib/themes/helpers' ),
 	addTracking = require( './theme-options' ).addTracking,
 	actionLabels = require( './action-labels' ),
-	ThemesListSelectors = require( 'lib/themes/themes-list/selectors' ),
-	getCurrentUser = require( 'state/current-user/selectors' ).getCurrentUser;
+	ThemesListSelectors = require( 'state/themes/themes-list/selectors' );
 
 var Themes = React.createClass( {
-	propTypes: {
-		isLoggedOut: React.PropTypes.bool.isRequired
-	},
 
 	getInitialState: function() {
 		return {
-			selectedTheme: null,
-			selectedAction: null,
-		};
-	},
-
-	showSiteSelectorModal: function( action, theme ) {
-		this.setState( { selectedTheme: theme, selectedAction: action } );
+			showPreview: false
+		}
 	},
 
 	togglePreview: function( theme ) {
-		const previewUrl = ThemeHelpers.getPreviewUrl( theme );
-		this.setState( { showPreview: ! this.state.showPreview, previewUrl: previewUrl, previewingTheme: theme } );
-	},
-
-	hideSiteSelectorModal: function() {
-		this.showSiteSelectorModal( null, null );
-	},
-
-	isThemeOrActionSet: function() {
-		return this.state.selectedTheme || this.state.selectedAction;
+		this.setState( { showPreview: ! this.state.showPreview, previewingTheme: theme } );
 	},
 
 	getButtonOptions: function() {
@@ -92,8 +72,7 @@ var Themes = React.createClass( {
 	},
 
 	render: function() {
-		var { dispatch } = this.props,
-			buttonOptions = this.getButtonOptions();
+		var buttonOptions = this.getButtonOptions();
 
 		return (
 			<Main className="themes">
@@ -124,13 +103,6 @@ var Themes = React.createClass( {
 					tier={ this.props.tier }
 					queryParams={ this.props.queryParams }
 					themesList={ this.props.themesList } />
-				{ this.isThemeOrActionSet() && <ThemesSiteSelectorModal
-					name={ this.state.selectedAction /* TODO: Can we get rid of this prop? */ }
-					label={ actionLabels[ this.state.selectedAction ].label }
-					header={ actionLabels[ this.state.selectedAction ].header }
-					selectedTheme={ this.state.selectedTheme }
-					onHide={ this.hideSiteSelectorModal }
-					action={ bindActionCreators( Action[ this.state.selectedAction ], dispatch ) }
 				/> }
 			</Main>
 		);
@@ -142,8 +114,7 @@ export default connect(
 		props,
 		{
 			queryParams: ThemesListSelectors.getQueryParams( state ),
-			themesList: ThemesListSelectors.getThemesList( state ),
-			isLoggedOut: ! getCurrentUser( state )
+			themesList: ThemesListSelectors.getThemesList( state )
 		}
 	)
 )( Themes );
